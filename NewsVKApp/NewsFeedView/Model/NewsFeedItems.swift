a7c37b6ae30e208c9244fd3502ea8773f19f3a5f
Baha Sadyr <>
2024-12-05 18:49:01 +0500

    Stop tracking xcschememanagement.plist

---
+++ NewsVKApp/NewsFeedView/Model/NewsFeedItems.swift
@@ -0,0 +1,16 @@
+//
+//  NewsFeedItems.swift.swift
+//  NewsVKApp
+//
+//  Created by Baha Sadyr on 12/5/24.
+//
+
+import Foundation
+
+struct NewsFeedItems: Codable {
+    var title: String
+    var desc: String
+    var header: String
+    var mainImage: String
+}
+

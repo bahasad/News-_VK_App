a343e9e8707f47e9c75f8492069b984517a0381d
Baha Sadyr <>
2024-12-10 16:40:14 +0500

    added scrolling for the whole page in newsfeed VC

--- NewsVKApp/NewsFeedView/View/NewsFeedVC.swift
+++ NewsVKApp/NewsFeedView/View/NewsFeedVC.swift
@@ -75,6 +75,7 @@ class NewsFeedVC: UIViewController, NewsFeedVCProtocol {
     private lazy var activityIndicator: UIActivityIndicatorView = {
         $0.color = .gray
         $0.hidesWhenStopped = true
+        $0.translatesAutoresizingMaskIntoConstraints  = false
         return $0
     }(UIActivityIndicatorView(style: .large))
     
@@ -82,8 +83,28 @@ class NewsFeedVC: UIViewController, NewsFeedVCProtocol {
         $0.delegate = self
         $0.dataSource = self
         $0.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.reuseId)
+        $0.isScrollEnabled = false
         return $0
     }(UICollectionView(frame: .zero, collectionViewLayout: setLayout()))
+    
+    private lazy var scrollView: UIScrollView = {
+        $0.isScrollEnabled = true
+        $0.showsVerticalScrollIndicator = false
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIScrollView(frame: view.frame))
+    
+    private lazy var scrollContent: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .white
+        return $0
+    }(UIView())
+    
+    private lazy var headerView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .white
+        return $0
+    }(UIView())
     
     
     override func viewDidLoad() {
@@ -96,47 +117,80 @@ class NewsFeedVC: UIViewController, NewsFeedVCProtocol {
     }
     
     private func addSubviews() {
-        [ userImage, userName, menuIcon, searchBar, headerLabel, collectionView, activityIndicator ].forEach {
-            view.addSubview($0)
+        
+        view.addSubview(scrollView)
+        view.addSubview(activityIndicator)
+        scrollView.addSubview(scrollContent)
+        
+        [ userImage, userName, menuIcon, searchBar].forEach {
+            headerView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [ headerView, headerLabel, collectionView ].forEach {
+            scrollContent.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
     }
+
     
+    private var collectionViewHeightConstraint: NSLayoutConstraint?
+
     private func setConstraints() {
+        collectionViewHeightConstraint = collectionView.heightAnchor.constraint(equalToConstant: 0)
+        collectionViewHeightConstraint?.isActive = true
+
         NSLayoutConstraint.activate([
-            
+
             userImage.widthAnchor.constraint(equalToConstant: 35),
             userImage.heightAnchor.constraint(equalToConstant: 35),
-            userImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
-            userImage.centerYAnchor.constraint(equalTo: menuIcon.centerYAnchor),
-            
-            menuIcon.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            userImage.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 25),
+            userImage.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 10),
+
+            menuIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -25),
             menuIcon.widthAnchor.constraint(equalToConstant: 24),
             menuIcon.heightAnchor.constraint(equalToConstant: 24),
-            menuIcon.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            
+            menuIcon.centerYAnchor.constraint(equalTo: userImage.centerYAnchor),
+
             userName.leadingAnchor.constraint(equalTo: userImage.trailingAnchor, constant: 10),
             userName.trailingAnchor.constraint(equalTo: menuIcon.leadingAnchor, constant: -10),
             userName.centerYAnchor.constraint(equalTo: userImage.centerYAnchor),
-            
+
             searchBar.topAnchor.constraint(equalTo: userImage.bottomAnchor, constant: 15),
-            searchBar.leadingAnchor.constraint(equalTo: userImage.leadingAnchor),
-            searchBar.trailingAnchor.constraint(equalTo: menuIcon.trailingAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 25),
+            searchBar.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -25),
             searchBar.heightAnchor.constraint(equalToConstant: 40),
-            
-            headerLabel.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 25),
-            headerLabel.leadingAnchor.constraint(equalTo: searchBar.leadingAnchor),
-            headerLabel.trailingAnchor.constraint(equalTo: searchBar.trailingAnchor),
-            
-            collectionView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 25),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
+
+            headerView.topAnchor.constraint(equalTo: scrollContent.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: scrollContent.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: scrollContent.trailingAnchor),
+            headerView.bottomAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 15),
+
+            headerLabel.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 20),
+            headerLabel.leadingAnchor.constraint(equalTo: scrollContent.leadingAnchor, constant: 25),
+            headerLabel.trailingAnchor.constraint(equalTo: scrollContent.trailingAnchor, constant: -25),
+
+            collectionView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20),
+            collectionView.leadingAnchor.constraint(equalTo: scrollContent.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: scrollContent.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: scrollContent.bottomAnchor),
+
             activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            scrollContent.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            scrollContent.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            scrollContent.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            scrollContent.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            scrollContent.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
+
     
     private func getAllItems() {
         showLoadingIndicator()
@@ -156,15 +210,22 @@ class NewsFeedVC: UIViewController, NewsFeedVCProtocol {
         layout.scrollDirection = .vertical
         layout.minimumInteritemSpacing = 10
         layout.minimumLineSpacing = 20
-        layout.sectionInset = UIEdgeInsets(top: 10 , left: 10, bottom: 10, right: 10)
-        //layout.itemSize = CGSize(width: 100, height: 500)
-        let screenWidth = UIScreen.main.bounds.width
-        let cellWidth = (screenWidth - 50)
-        layout.itemSize = CGSize(width: cellWidth, height: 500)
-        
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 50, height: 500)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         return layout
     }
-    
+
+  
+//    
+//    func setLayout() -> UICollectionViewFlowLayout {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+//        layout.minimumLineSpacing = 10
+//        layout.minimumInteritemSpacing = 10
+//        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+//        return layout
+//    }
+
     private func showLoadingIndicator() {
         DispatchQueue.main.async {
             self.activityIndicator.startAnimating()
@@ -197,14 +258,24 @@ extension NewsFeedVC: UISearchBarDelegate {
 }
 
 extension NewsFeedVC {
-    
+ 
     func updateNewsFeed(with data: [NewsFeedItems]) {
         print("Fetched \(data.count) news to display")
         DispatchQueue.main.async {
             self.hideLoadingIndicator()
             self.collectionView.reloadData()
+            self.collectionView.layoutIfNeeded()
+            self.updateCollectionViewHeight()
         }
     }
+
+    private func updateCollectionViewHeight() {
+        self.collectionViewHeightConstraint?.constant = self.collectionView.contentSize.height
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
 }
 
 extension NewsFeedVC: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -252,6 +323,7 @@ extension NewsFeedVC: CollectionViewCellDelegate {
         cell.starBtn.isBookmarked = !isBookmarked
     }
 }
-//scrollview, - to do item (for the whole page to be scrollabel, not only the collection view!
+
+
 
 
